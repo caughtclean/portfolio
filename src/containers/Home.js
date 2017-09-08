60a1c38a14bbf249a2fe7b6e23304a09f359fe25
Nathan <eyerleyn@gmail.com>
2017-09-08 01:26:16 +0000

    first commit, added react router, set up basic home routes

---
+++ src/containers/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  static protoTypes = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Hi Home</h1>
+      </div>
+    );
+  }
+}
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import routes from './routes';
+
+ReactDOM.render(
+  routes,
+  document.getElementById('root')
+);
+
